41fc83384f86dcba86ea7fc038ba8e50779765f9
John Dana <john@danasd.com>
2023-04-17 08:25:22 -0600

    Add ProgramForm

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import "./index.css";
 import Login from './routes/Login'
 import CityForm from './routes/cities/CityForm'
 import SelectProgram from './routes/cities/SelectProgram'
+import ProgramForm from './routes/cities/programs/ProgramForm'
 import { createBrowserRouter } from "react-router-dom";
 
-// QQS:  ASSIGNMENT 1: I've been unable to get redirectTo: to work. 
+// QQS:  ASSIGNMENT 1: Take a look at src/routes.js. (index.js uses this file for all routes.) I've been unable to get redirectTo: to work. 
 // QQN:  I've effectively "squashed" the directory structure. Rather than going with the Ember-style 
 //      cities/city/programs/program, I went with cities/programs. I think this will work. 
-// QQN:  Notice how I have to alias components to naming conflicts. Let me know if you have a better way of doing this. 
 const router = createBrowserRouter([
   {
     path: '/',
@@ -29,6 +29,14 @@ const router = createBrowserRouter([
   {
     path: '/cities/:cityId',
     element: <SelectProgram />,
+  },
+  {
+    path: '/cities/:cityId/programs/create',
+    element: <ProgramForm />,
+  },
+  {
+    path: '/cities/:cityId/programs/:programId/update',
+    element: <ProgramForm />,
   },
 ]);
 export default router
