bfae1651dc6b7ffc26398ab4348c3bb88a2b45a3
John Dana <john@danasd.com>
2023-04-17 17:19:04 -0600

    put comments

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import { RouterProvider } from "react-router-dom";
 import { ApiClient, ApiProvider } from 'jsonapi-react'
 import schema from './schema'
  
+// QQ Take a look at my configuration for react-router-dom and jsonapi-react. I think I got it. 
+// QQ I dropped the router in a separate route.js file to clean up index.js. 
+// QQ I changed the apiUrl. I also toggled React.js to "serve" port 3001 rather than 3000 using an .env file.
+// QQ Presumably, I'll need to add some dynamic functionality to tweak the apiUrl based on the environment (dev, staging, prod). 
 const client = new ApiClient({
-  apiUrl: 'https://api.example.com',
+  apiUrl: 'http://127.0.0.1:3000',
   schema
 });
 
