02cd9ad2cee6841187e74a16b28b8b16209fd50a
John Dana <john@danasd.com>
2023-04-24 14:13:09 -0600

    Improvements to SelectProgram

--- src/routes.js
+++ src/routes.js
@@ -6,12 +6,6 @@ import SelectProgram from './routes/cities/SelectProgram'
 import ProgramForm from './routes/cities/programs/ProgramForm'
 import { createBrowserRouter } from "react-router-dom";
 
-// QQ:  ASSIGNMENT 1: Take a look at src/routes.js. (index.js uses this file for all routes.) I've been unable to get redirectTo: to work. 
-// QQ:  I've effectively "squashed" the directory structure. Rather than going with the Ember-style 
-//      cities/city/programs/program, I went with cities/programs. I think this will work. 
-// QQ:  Just like the Ember.js router, I'm going to "split the namespace," with the "bottom-half" of the namespace looking like this: 
-//      '/:programId/properties', '/:programId/properties/:propertyId/Hub', '/:programId/properties/:propertyId/PropertyForm/', 
-//      '/:programId/properties/:propertyId/PropertyForm/'
 const router = createBrowserRouter([
   {
     path: '/',
--- src/routes/cities/SelectProgram.js
+++ src/routes/cities/SelectProgram.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { getCityPrograms } from "../../apiClient/cities";
 
-const SelectProgram = () => (
-  <div>
-    <h1>SelectProgram</h1>
-    <nav>
-      <Link to="/">Program</Link>
-    </nav>
-  </div>
-);
+const SelectProgram = () => {
+  const { cityId } = useParams();
+  getCityPrograms(cityId).then((p) => {
+    console.log("cp", p);
+  });
+  return (
+    <div>
+      <h1>SelectProgram</h1>
+      <nav>
+        <Link to="/">Program</Link>
+      </nav>
+    </div>
+  );
+};
 
 export default SelectProgram;
