2738e70308278d99ae746d6b57f42e976cb21c5d
John Dana <john@danasd.com>
2023-04-24 17:51:37 -0600

    add react form control

--- src/routes/cities/CityForm.js
+++ src/routes/cities/CityForm.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getCity, createCity, updateCity } from "../../apiClient/cities";
+import { useForm } from "react-hook-form";
 
 const handleCreate = () => {
   createCity({
@@ -12,28 +13,46 @@ const handleCreate = () => {
   });
 };
 
-const handleUpdate = () => {
-  updateCity(1, {
-    id: 1,
-    cityName: "jazzercise" + new Date().getSeconds(),
-  });
+const submitForm = (data) => {
+  if (data.id) {
+    updateCity(1, data);
+  } else {
+    createCity({
+      cityName: "jazzercise",
+      state: "CO",
+      logoMain: "asdf",
+      createdBy: "me",
+      updatedBy: "me",
+      ...data,
+    });
+  }
+};
+
+const initCityFormData = async (cityId) => {
+  if (cityId) {
+    console.log(cityId);
+    return getCity(cityId);
+  } else {
+    return {};
+  }
 };
 
 const CityForm = () => {
   const [city, setCity] = useState([]);
   const { cityId } = useParams();
-  useEffect(() => {
-    console.log("cityId =", cityId);
-    getCity(cityId).then((city) => {
-      setCity(city);
-    });
-  }, []);
+  const { register, handleSubmit } = useForm({
+    defaultValues: () => initCityFormData(cityId),
+  });
 
   return (
     <div>
       <h1>City Form</h1>
-      <input type="button" value="create" onClick={handleCreate} />
-      <input type="button" value="update" onClick={handleUpdate} />
+      {/* <input type="button" value="create" onClick={handleCreate} />
+      <input type="button" value="update" onClick={handleUpdate} /> */}
+      <input {...register("cityName")} />
+      <button type="button" onClick={handleSubmit(submitForm)}>
+        submit
+      </button>
     </div>
   );
 };
