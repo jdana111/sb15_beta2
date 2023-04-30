d763ca4ef6291ab772b045f3d597134bd30590a0
John Dana <john@danasd.com>
2023-04-30 09:13:48 -0600

    Cleanup on config.js

--- src/apiClient/config.js
+++ src/apiClient/config.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 import { Deserializer } from "jsonapi-serializer";
 
+// NATHO did this.
+// a) What does the single vertical bar mean?
+// b) process.env.API_URL is pulling a URL with a port of 3001 from SOMEWHERE. 
+// const API_URL = process.env.API_URL | "http://127.0.0.1:3000/api";
 const API_URL = "http://127.0.0.1:3000/api";
 
 const instance = axios.create({
